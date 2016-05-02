b1d390a390bd9c036a29cfed9b3d99d3795846a6
William Yang <wyang@macaroni-02.cs.wisc.edu>
2016-05-02 17:54:35 -0500

    part 3 added minimum missed

--- project3.m
+++ project3.m
@@ -1,4 +1,5 @@
 mu = 5e-4;
+minMissed = size(Mtune,1);
 for i = 1:29
   for j = (i+1):30
     Msubtrain = Mtrain(:,[i j]);
@@ -6,6 +7,10 @@ for i = 1:29
     Msubtune = Mtune(:,[i j]);
     Bsubtune = Btune(:,[i j]);
     numMissed = evaluate(mu,Msubtrain,Bsubtrain,Msubtune,Bsubtune);
+    if numMissed < minMissed
+      minMissed = numMissed;
+    end
     fprintf('atts %2d %2d: misclass %3d\n',i,j, numMissed);
   end
 end
+minMissed
