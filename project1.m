49a32caa6f20117061ea97eae767de6f58051cf3
William Yang <wyang@macaroni-07.cs.wisc.edu>
2016-05-01 23:00:20 -0500

    editted part1 output

--- project1.m
+++ project1.m
@@ -1,11 +1,8 @@
 % ********** 1. **********
 % get data
 [train, tune, test] = getdata('wdbc.data', 30);
-Btrain_indicies = find(train(:,1) == 66);
-Mtrain_indicies = find(train(:,1) == 77);
-Btrain = train(Btrain_indicies,2:31);
-Mtrain = train(Mtrain_indicies,2:31);
-
+Btrain = train(find(train(:,1) == 66), 2:31);
+Mtrain = train(find(train(:,1) == 77), 2:31);
 mu = 0.0001;
 % fit the model
 [w gamma obj] = fitModel(mu, Mtrain, Btrain)
