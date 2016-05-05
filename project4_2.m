08c2969584e58ef189c155e39281d1be42d7d344
William Yang <wyang@galapagos-24.cs.wisc.edu>
2016-05-04 22:10:16 -0500

    cocncise representation of R2 sets

--- project4_2.m
+++ project4_2.m
@@ -2,4 +2,8 @@ Btest = test(find(test(:,1) == 66), 2:31);
 Mtest = test(find(test(:,1) == 77), 2:31);
 mu = 5e-4;
 idxs = [24 25];
-numMissed = evaluate(mu,Mtrain(:,idxs),Btrain(:,idxs),Mtest(:,idxs),Btest(:,idxs))
+Mtrain2 = Mtrain(:,idxs);
+Btrain2 = Btrain(:,idxs);
+Mtest2 = Mtest(:,idxs);
+Btest2 = Btest(:,idxs);
+numMissed = evaluate(mu,Mtrain2,Btrain2,Mtest2,Btest2)
